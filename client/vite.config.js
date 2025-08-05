730d5e6ee6ac0ce8c312ebff9b6708b5379724d0
D0dgerJ <d0dgerj89@gmail.com>
2025-08-06 02:35:51 +0300

    added the ability to download files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // ✅ добавляем path
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ✅ алиас @ на src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000/api/v1',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
