58f6d554706a1184d01ea4723421caff36cf7623
D0dgerJ <d0dgerj89@gmail.com>
2025-09-07 00:34:35 +0300

    Fixed display of likes and comments on the profile page

--- server/application/use-cases/post/getUserPostsByUsername.ts
+++ server/application/use-cases/post/getUserPostsByUsername.ts
@@ -3,9 +3,12 @@ import prisma from "../../../infrastructure/database/prismaClient.ts";
 export const getUserPostsByUsername = async (username: string) => {
   const user = await prisma.user.findUnique({
     where: { username },
+    select: { id: true },
   });
 
-  if (!user) throw new Error("User not found");
+  if (!user) {
+    throw new Error("User not found");
+  }
 
   const posts = await prisma.post.findMany({
     where: { userId: user.id },
@@ -13,8 +16,20 @@ export const getUserPostsByUsername = async (username: string) => {
     include: {
       user: {
         select: {
+          id: true,
           username: true,
           profilePicture: true,
+        },
+      },
+      likes: {
+        select: {
+          userId: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          comments: true,
         },
       },
     },
